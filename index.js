ef2ca4d1bfc7c5ef53e054dc54512f00ff03ca01
Bill Nizeyimana <bni@escaux.com>
2023-07-09 23:20:13 +0200

    fix metadata

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import { AccessToken, RoomServiceClient } from "livekit-server-sdk";
 dotenv.config();
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const DEFAULT_ROOM_NAME = "#MainOffice";
 app.post("/connection_details", async (req, res) => {
-    const {username,metadata} = req.body
+  const { username, metadata } = req.body;
   const apiKey = process.env.LIVEKIT_API_KEY;
   const apiSecret = process.env.LIVEKIT_API_SECRET;
   const wsUrl = process.env.LIVEKIT_WS_URL;
@@ -30,12 +30,17 @@ app.post("/connection_details", async (req, res) => {
   } catch {
     // If participant doesn't exist, we can continue
   }
-    at.addGrant({ DEFAULT_ROOM_NAME, canUpdateOwnMetadata:true, canPublishData:true,roomJoin: true, canPublish: true, canSubscribe: true });
-    if (metadata) {
-        at.metadata = JSON.stringify({ character });
-    }
- 
-
+  at.addGrant({
+    DEFAULT_ROOM_NAME,
+    canUpdateOwnMetadata: true,
+    canPublishData: true,
+    roomJoin: true,
+    canPublish: true,
+    canSubscribe: true,
+  });
+  if (metadata) {
+    at.metadata = JSON.stringify({ character });
+  }
   res.send(JSON.stringify({ token: at.toJwt(), ws_url: wsUrl }));
 });
 
